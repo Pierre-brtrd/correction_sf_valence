5f2c33f547e8702c9ac769396e36f428530aff0e
pierre.bertrand.professionnel <pierre.bertrand.professionnel@gmail.com>
2024-07-30 16:13:44 +0200

    feat(taxe): add crud for taxe

---
+++ src/Entity/Taxe.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Traits\EnableTrait;
+use App\Repository\TaxeRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: TaxeRepository::class)]
+class Taxe
+{
+    use EnableTrait;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Length(max: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    #[Assert\NotBlank]
+    #[Assert\PositiveOrZero]
+    private ?float $rate = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getRate(): ?float
+    {
+        return $this->rate;
+    }
+
+    public function setRate(float $rate): static
+    {
+        $this->rate = $rate;
+
+        return $this;
+    }
+}
