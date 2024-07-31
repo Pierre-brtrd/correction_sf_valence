e86ac5b77851a3ef1411fb34368bc22f16bc46e0
pierre.bertrand.professionnel <pierre.bertrand.professionnel@gmail.com>
2024-07-31 14:04:17 +0200

    feat(admin): add admin base template

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('admin', './assets/admin.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
